5a7bd1bf56c1b1ebf6c447a54e0eacad9fb5dd0a
maekawa19021-gif <maekawa19021@gmail.com>
2026-07-08 17:01:59 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -11,10 +11,10 @@
    ============================================================ */
 window.SITE_CONFIG = {
   FORM_URLS: {
-    join:   "",  // ①後援会入会
-    donate: "",  // ②寄付
-    party:  "",  // ③党員・サポーター
-    full:   ""   // ④寄付＋党員・サポーター
+    join:   "https://docs.google.com/forms/d/e/1FAIpQLSeNnKNgI4qroVGcVkSkW5tWOzl1apavfZZl_tQ3hisZEm9PGw/viewform",  // ①後援会入会
+    donate: "https://docs.google.com/forms/d/e/1FAIpQLSdyDJyTaezU7b_PqQR0XcVhfqKxeFmZRfVica63v79JmROGQg/viewform",  // ②寄付
+    party:  "https://docs.google.com/forms/d/e/1FAIpQLSfzzAo9KygE25iVTE4yMJdlQ1Pb_zcCGZyrTuJq28AGYSih3w/viewform",  // ③党員・サポーター
+    full:   "https://docs.google.com/forms/d/e/1FAIpQLSeQaoYPuZS6UmKGlCgZ-AdvGf4NwdEdawXYFYTCGvx8PQrblg/viewform"   // ④寄付＋党員・サポーター
   },
   LINE_URL: ""
 };
