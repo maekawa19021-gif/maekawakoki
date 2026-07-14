c676d682c8c39584bc9ef48787c90a2167c6af2e
maekawa19021-gif <maekawa19021@gmail.com>
2026-07-14 12:35:40 +0900

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@
    ============================================================ */
 window.SITE_CONFIG = {
   // ★ここだけ、Apps Scriptをデプロイした後に必ず貼り付けてください。
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwchzlQJe4SoLjkMiw0q6u_UF1zYGfH5mAX7GtAyeJJnInvGQHnjFlT_0cWHVe46R3OiA/exec,
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwchzlQJe4SoLjkMiw0q6u_UF1zYGfH5mAX7GtAyeJJnInvGQHnjFlT_0cWHVe46R3OiA/exec",
 
   FORM_URLS: {
     // setupForm() 実行後に発行される「回答者用フォーム」のURLを貼り付けます。
